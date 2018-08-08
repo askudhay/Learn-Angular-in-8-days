f155baf85ba178a47af5dd26df9a0f3079b9ef00
Udhay <systemudhaya@gmail.com>
2018-08-08 07:14:45 -0400

    Day 2 Assignment..

---
+++ Day 2/Assignment/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Assignment';
+  state: Boolean = false;
+  bulbImg: String = 'assets/bulb_off.png';
+  switch(currState: Boolean) {
+    this.state = currState;
+    this.bulbImg = this.state ? 'assets/bulb_on.png' : 'assets/bulb_off.png';
+  }
+}
