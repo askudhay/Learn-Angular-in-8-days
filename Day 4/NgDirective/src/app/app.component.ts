6b52010a28cb13a9f090877a53c5b1622603688b
Udhay <systemudhaya@gmail.com>
2018-08-09 08:32:03 -0400

    Day 4 code

--- Day 4/NgDirective/src/app/app.component.ts
+++ Day 4/NgDirective/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Student } from './student';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NgDirective';
+  isServiceUnavailable: Boolean = false;
+  plInventors = ['Dennis Ritchie', 'Bjarne stroustrup', 'James_Gosling'];
+  students = [{ 'sId': 345, 'sName': 'Kumar', 'sDistinction': false }, { 'sId': 123, 'sName': 'Udhay', 'sDistinction': false }, {
+    'sId': 567, 'sName': 'John', 'sDistinction' : true
+  }, {'sId': 789, 'sName': 'Kelvis', 'sDistinction' : false}];
+
+  studentsArr: Array<Student> = [new Student(123, 'Udhay', true),
+            new Student(234, 'Kumar', false)];
+
+  addStudent() {
+    const sObj = new Student(44, 'Dan', true);
+    this.studentsArr.push(new Student(1, 'Vinod', false));
+    this.studentsArr.push(sObj);
+  }
 }
---
+++ Day 4/NgDirective/src/app/student.ts
@@ -0,0 +1,11 @@
+export class Student {
+    studId: number;
+    studName: String;
+    studDestinction: Boolean;
+
+    constructor(sId, sName, sDistnct) {
+        this.studId = sId;
+        this.studName = sName;
+        this.studDestinction = sDistnct;
+    }
+}
